e74fac18caed9cb25fa32c829a9c6599a80c68a0
Jonathan Ortega Morales <jonortmor@gmail.com>
2026-05-04 16:16:47 +0000

    estable

---
+++ models/stagging/google_sheets/stg_google_sheets__budget_merge.sql
@@ -0,0 +1,33 @@
+{{ config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='_row'
+) }}
+
+WITH stg_budget_products AS (
+    SELECT * 
+    FROM {{ source('google_sheets', 'budget') }}
+),
+
+renamed_casted AS (
+    SELECT
+          _row
+        , month
+        , quantity
+        , _fivetran_synced
+    FROM stg_budget_products
+),
+
+deduped AS (
+    SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY _row ORDER BY _fivetran_synced DESC) AS rn
+    FROM renamed_casted
+)
+
+SELECT _row, month, quantity, _fivetran_synced
+FROM deduped 
+WHERE rn = 1
+
+{% if is_incremental() %}
+    AND _fivetran_synced > (SELECT MAX(_fivetran_synced) FROM {{ this }})
+{% endif %}
